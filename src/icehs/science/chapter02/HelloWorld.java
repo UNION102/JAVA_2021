775edd1d55c5b38e31318f5f900e54ada79512b1
UNION <pih009100@naver.com>
2021-05-11 23:22:43 +0900

    Chapter03

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,10 +1,19 @@
 package icehs.science.chapter02;
-
+/* Hello World 
+ * v .01 by Park I Hyeon
+ * 2021.05.11 update
+ */
+/**
+ * 
+ * @author parks
+ *
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("자바를 자바라");//sysout Ctrl+Space
+		System.out.println("자바를 자바라"); //sysout Ctrl+Space
+		System.out.println("운서동 살기 좋아요...");
 	}
 
 }
