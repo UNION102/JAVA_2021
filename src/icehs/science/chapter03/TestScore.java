2ba11b013c6ca8577632b13d5de88806deb128dc
UNION <pih009100@naver.com>
2021-05-11 23:25:07 +0900

    Chapter03

---
+++ src/icehs/science/chapter03/TestScore.java
@@ -0,0 +1,13 @@
+package icehs.science.chapter03;
+
+public class TestScore {
+	public static void main(String[] args) {
+		int JavaProgrammingScore = 100;
+		int sqlScore = 80;
+		int servletJspScore = 85;
+		
+		System.out.println(JavaProgrammingScore);
+		System.out.println(sqlScore);
+		System.out.println(servletJspScore);
+	}
+}
