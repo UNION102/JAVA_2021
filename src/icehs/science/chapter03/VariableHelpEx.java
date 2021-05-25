b4ed3417949e1b14de17236aa6f52039079bbc73
UNION <pih009100@naver.com>
2021-05-25 21:42:36 +0900

    Chapter05

---
+++ src/icehs/science/chapter03/VariableHelpEx.java
@@ -0,0 +1,15 @@
+package icehs.science.chapter03;
+
+public class VariableHelpEx {
+	public static void main(String[] args) {
+		
+		int int1 = 1234;
+		int first;
+		int class;
+		int var;
+		int var;
+		int number;
+		
+		System.out.println(number);
+	}
+}
